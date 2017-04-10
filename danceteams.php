58682ee02a3ef0b2e6f77cd7bdc774010a208d48
Janiza G <hi@janizagesmundo.com>
2017-04-10 14:48:47 -0400

    Add files via upload

---
+++ danceteams.php
@@ -0,0 +1,93 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Dance Teams</title>
+<link rel="icon" href="images/favicon.gif" type="image/gif" sizes="16x16">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+<meta name="description" content="Dance Teams">
+<meta name="keywords" content="east coast dancers, new jersey dancers, new york dancers, hip hop, community, dance videos, classes">
+<meta name="author" content="Janiza Gesmundo">
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div id="wrapper">
+<header><h1>EAST COAST DANCE COMMUNITY</h1></header>
+
+<nav class="hidden-print">
+<ul>
+<li><a href="http://eastcoastdancers.com/">Home</a></li>
+<li><a href="http://eastcoastdancers.com/about.html">Webmaster</a></li>
+<li><a href="http://eastcoastdancers.com/dancereel.html">My Dance Reel</a></li>
+<li><a href="http://eastcoastdancers.com/danceteams.php">Dance Teams</a></li>
+<li><a href="http://eastcoastdancers.com/chessflex.html">Chessboard</a></li>
+<li><a href="http://cis3630.org/student-webpages.php">Student Page</a></li>
+<li><a href="http://www.mhncl.com/">Mengyuan's Site</a></li>
+<li><a href="https://www.google.com/webmasters/tools/mobile-friendly/?url=http%3A%2F%2Feastcoastdancers.com%2Fdanceteams.php">Mobile Test</a></li>
+<li><a href="https://validator.w3.org/nu/?doc=http%3A%2F%2Feastcoastdancers.com%2Fdanceteams.php">Validator</a></li>
+<li><a href="http://eastcoastdancers.com/traffic.html">Traffic Report</a></li>
+<li><a href="http://eastcoastdancers.com/sitemap.html">Site Map</a></li>
+</ul>
+</nav>
+
+
+<main>
+<hr/>
+<h2 class="center">COMPETING TEAMS</h2>
+<ul>
+<li><a href="https://www.facebook.com/pusomodern/">PUSO Modern</a> (New York, NY) </li>
+<li><a href="https://www.facebook.com/TheNeighborsDanceTeam">The Neighbors</a> (New York, NY) </li>
+<li><a href="https://www.facebook.com/ufpdance">UFP Dance</a> (Matawan, NJ) </li>
+<li><a href="http://www.iamphresh.com">Academy of Phresh</a> (Philadelphia, PA) </li>
+<li><a href="https://www.facebook.com/arhythmco/?fref=ts">ARC &#8211; A Rhythm Company</a> (Bergenfield, NJ) </li>
+<li><a href="http://www.cohedance.com">COhe</a> (Denver, CO)** </li>
+<li><a href="https://www.facebook.com/OutburstDance/?fref=ts">Outburst Dance Company</a> (New York, NY) </li>
+<li><a href="http://www.staticnoyzedance.com">Static Noyze Dance Company   </a>(Boston, MA)  </li>
+<li><a href="https://www.facebook.com/wearethesociety/?fref=ts">The Society</a> (New Brunswick, NJ) </li>
+<li><a href="https://www.facebook.com/ANMSDancecrew/?ref=br_rs">AnoNYmous Dance Crew</a> (Manhattan, NY) </li>
+<li><a href="https://www.instagram.com/k9collectiveto/">K9 Collective</a> (Toronto, Canada)** </li>
+<li><a href="http://www.thepuzzleleaguedance.com">the PieceMakers</a> (Chicago. Il)** </li>
+<li><a href="https://www.facebook.com/UniqueMovementDanceCrew">Unique Movement</a> (New York, NY) </li>
+<li><a href="https://www.facebook.com/TheClutchCollective/?hc_ref=NEWSFEED&amp;fref=nf">The Clutch Collective</a> (Camden, NJ) </li>
+<li><a href="https://www.facebook.com/mintdanceco">Mint Dance Company</a> (New York, NY) </li>
+<li><a href="https://www.facebook.com/synchronicdanceteam/">Synchronic Dance Team</a> (New York, NY) </li>
+<li><a href="https://www.facebook.com/rapsdancetroupe?fref=ts">Raps Dance-Troupe</a> &#8211; <em>RDT</em> (New Brunswick, NJ)</li>
+</ul>
+**visiting teams
+<br><br>
+<hr>
+<h2 class="center">EXHIBITION TEAMS</h2>
+<ul>
+<li><a href="https://www.facebook.com/VEN15ladies-278479105521329/">VEN15 Ladies   </a>(New Jersey)</li>
+<li><a href="https://www.facebook.com/iNSPIREDdancecrew/?fref=ts">iNSPIRED! Dance Crew </a>(New Jersey)</li>
+<li><a href="https://www.facebook.com/epicmotiondancecompany/">EPIC Motion Dance Company </a>(New York, NY)</li>
+<li><a href="https://www.facebook.com/serendipitydancetroupe/">Serendipity Dance Troupe </a>(New York, NY)</li>
+<li><a href="https://www.facebook.com/THEMdanceco/?fref=ts">THEM </a>(New York, NY)</li>
+<li><a href="https://www.facebook.com/2sdaysfn/?fref=ts">2sdays </a>(New York, NY)</li>
+<li>Lucy Stoners (New Brunswick, NJ)</li>
+<li><a href="https://www.facebook.com/edsonmaldonado1?fref=ts">Edson Maldonado </a>(New Jersey)</li>
+<li><a href="https://www.facebook.com/projectd.nj/?fref=ts">Project D Dance Company </a>(New Jersey)</li>
+</ul>
+
+<hr>
+<h2 class="center">Submitted Dance Teams</h2>
+ <ol><li><a href="">FUSION Modern</a></li><li><a href="https://www.youtube.com/watch?v=cJfUNYo4_Ic">2sdays</a></li><li><a href="https://www.facebook.com/HipHopTheoryTC/timeline">Hip Hop Theory The Company</a></li><li><a href="">Banana Tree</a></li><li><a href="http://justinbieber.com/">Justin Bieber</a></li><li><a href="justinpangilinan.com">Justin's team</a></li><li><a href="">Test</a></li><li><a href="">awesome dance team</a></li><li><a href="">The Teddy Dance Team</a></li><li><a href="cis3630.org">Troy's Team</a></li><li><a href="www.google.com">a</a></li><li><a href="blazehoe420.org">cacaflaca</a></li><li><a href="Me">Hello</a></li><li><a href="">dance team</a></li></ol><hr>
+<h2 class="center"><a href="add-team.php">Submit A New Dance Team!</a></h2>
+<hr>
+
+<br>
+ 
+</main>
+
+
+
+
+
+<footer>
+Copyright &copy; 2016 EastCoastDancers.Com<br>
+<a href="mailto:hi@janizagesmundo.com">hi@janizagesmundo.com</a>
+</footer>
+</div>
+</body>
+</html>
